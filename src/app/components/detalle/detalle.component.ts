8ab4feac0e6277a4d3cfaa9742ced1bd8ce9d36a
Dani Te <daniel.te.mar@gmail.com>
2023-05-26 00:29:43 +0200

    Detalles de pelicula html y tsconfig para no inicializar

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { PeliculaDetalle } from 'src/app/interfaces/interfaces';
 import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
@@ -10,12 +11,15 @@ export class DetalleComponent implements OnInit {
 
   @Input() id: any;
 
+  pelicula: PeliculaDetalle;
+
   constructor(private moviesService: MoviesService) { }
 
   ngOnInit() {
     this.moviesService.getPeliculaDetalle(this.id)
       .subscribe(resp => {
         console.log(resp);
+        this.pelicula = resp;
       })
 
     this.moviesService.getActoresPelicula(this.id)
