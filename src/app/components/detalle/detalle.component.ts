a9d5186f3eb1bb1fbfcca08eb9d807d5fbb6b892
Dani Te <daniel.te.mar@gmail.com>
2023-05-26 00:15:18 +0200

    información de pelicula y actores

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,18 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   styleUrls: ['./detalle.component.scss'],
 })
-export class DetalleComponent  implements OnInit {
+export class DetalleComponent implements OnInit {
 
   @Input() id: any;
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) { }
 
   ngOnInit() {
-    
-  }
+    this.moviesService.getPeliculaDetalle(this.id)
+      .subscribe(resp => {
+        console.log(resp);
+      })
 
+    this.moviesService.getActoresPelicula(this.id)
+      .subscribe(resp => {
+        console.log(resp);
+      })
+  }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { RespuestaMDB } from '../interfaces/interfaces';
+import { PeliculaDetalle, RespuestaCredits, RespuestaMDB } from '../interfaces/interfaces';
 import { environment } from 'src/environments/environment';
 
 const URL = environment.url;
@@ -54,5 +54,14 @@ export class MoviesService {
     return this.ejecutarQuery<RespuestaMDB>(query);
 
   }
+
+  getPeliculaDetalle(id: number){
+    return this.ejecutarQuery<PeliculaDetalle>(`/movie/${id}?a=1`);
+  }
+
+  getActoresPelicula(id: number){
+    return this.ejecutarQuery<RespuestaCredits>(`/movie/${id}/credits?a=1`);
+  }
+
 
 }
